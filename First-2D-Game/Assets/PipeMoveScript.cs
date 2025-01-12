0b3d795a9d38474a13686625989425a4daebc0f1
zirc444 <christianziegler997@gmail.com>
2025-01-13 00:03:11 +0100

    Add Pipes will be deleted when they reach -45 on the x  axis

--- First-2D-Game/Assets/PipeMoveScript.cs
+++ First-2D-Game/Assets/PipeMoveScript.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PipeMoveScript : MonoBehaviour
 {
     public float moveSpeed = 5;
-
+    public float deadZone = -45;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,5 +15,10 @@ public class PipeMoveScript : MonoBehaviour
     {
         transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime; 
         
+        if (transform.position.x < deadZone)
+        {
+            Debug.Log("Pipe Deleted");
+            Destroy(gameObject);
+        }
     }
 }
