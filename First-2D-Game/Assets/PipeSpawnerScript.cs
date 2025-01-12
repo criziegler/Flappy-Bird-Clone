018c741014b41c7aefda3d5724be144616be1ee3
zirc444 <christianziegler997@gmail.com>
2025-01-12 23:46:37 +0100

    Add Pipe Spawner Random height

--- First-2D-Game/Assets/PipeSpawnerScript.cs
+++ First-2D-Game/Assets/PipeSpawnerScript.cs
@@ -6,6 +6,7 @@ public class PipeSpawnerScript : MonoBehaviour
     public GameObject pipe;
     public float spawnRate = 2; 
     private float timer = 0;
+    public float heightOffset = 10; 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -30,6 +31,8 @@ public class PipeSpawnerScript : MonoBehaviour
 
     void spawnPipe()
     {
-        Instantiate(pipe, transform.position, transform.rotation);
+        float lowestPoint = transform.position.y - heightOffset;
+        float highestPoint = transform.position.y + heightOffset; 
+        Instantiate(pipe, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
     }
 }
