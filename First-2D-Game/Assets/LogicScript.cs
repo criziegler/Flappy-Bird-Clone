c2d6b89697d9f11f3a0f2775e68673408478f22e
zirc444 <christianziegler997@gmail.com>
2025-01-13 21:34:31 +0100

    +Add Score when Player goes through Pipes

--- First-2D-Game/Assets/LogicScript.cs
+++ First-2D-Game/Assets/LogicScript.cs
@@ -6,8 +6,8 @@ public class LogicScript : MonoBehaviour
    public int playerScore;
    public Text scoreText;
 
-    [ContextMenu("Increase Score")]
-   void addScore()
+   [ContextMenu("Increase Score")]
+   public void addScore()
    {
         playerScore = playerScore + 1;
         scoreText.text = playerScore.ToString();
